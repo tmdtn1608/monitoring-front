b7c4f8724506bb483b78b8ff4d66b04da9a5bc8a
BaekSeungSu <tmdtn1608@gmail.com>
2024-11-02 18:00:29 +0900

    라이센스 화면 진행중

--- src/Component/License.js
+++ src/Component/License.js
@@ -1,155 +1,82 @@
 import React, { useEffect, useState, createContext } from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
 import '../CSS/License.css'
 
-function createData(name, calories, fat, carbs, protein, price) {
-    return {
-        name,
-        calories,
-        fat,
-        carbs,
-        protein,
-        price,
-        history: [
-            {
-            date: '2020-01-05',
-            customerId: '11091700',
-            amount: 3,
-            },
-            {
-            date: '2020-01-02',
-            customerId: 'Anonymous',
-            amount: 1,
-            },
-        ],
-    };
-}
-
-function Row(props) {
-    const { row } = props;
-    const [open, setOpen] = React.useState(false);
-  
-    return (
-        <React.Fragment>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                <TableCell>
-                    <IconButton
-                    aria-label="expand row"
-                    size="small"
-                    onClick={() => setOpen(!open)}
-                    >
-                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell component="th" scope="row">
-                    {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-            <TableRow>
-            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                <Box sx={{ margin: 1 }}>
-                    <Typography variant="h6" gutterBottom component="div">
-                    History
-                    </Typography>
-                    <Table size="small" aria-label="purchases">
-                    <TableHead>
-                        <TableRow>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Customer</TableCell>
-                        <TableCell align="right">Amount</TableCell>
-                        <TableCell align="right">Total price ($)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {row.history.map((historyRow) => (
-                        <TableRow key={historyRow.date}>
-                            <TableCell component="th" scope="row">
-                            {historyRow.date}
-                            </TableCell>
-                            <TableCell>{historyRow.customerId}</TableCell>
-                            <TableCell align="right">{historyRow.amount}</TableCell>
-                            <TableCell align="right">
-                            {Math.round(historyRow.amount * row.price * 100) / 100}
-                            </TableCell>
-                        </TableRow>
-                        ))}
-                    </TableBody>
-                    </Table>
-                </Box>
-                </Collapse>
-            </TableCell>
-            </TableRow>
-      </React.Fragment>
-    );
-}
-  
-Row.propTypes = {
-    row: PropTypes.shape({
-        calories: PropTypes.number.isRequired,
-        carbs: PropTypes.number.isRequired,
-        fat: PropTypes.number.isRequired,
-        history: PropTypes.arrayOf(
-            PropTypes.shape({
-                amount: PropTypes.number.isRequired,
-                customerId: PropTypes.string.isRequired,
-                date: PropTypes.string.isRequired,
-            }),
-        ).isRequired,
-        name: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        protein: PropTypes.number.isRequired,
-    }).isRequired,
-};
-  
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-    createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-    createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-    createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
-  
-
 function License() {
+    // API 값 저장을 위한 State
+    const [licenseList, setLicenseList] = useState(null);
+    // 로딩 및 에러상태를 관리하기 위한 State
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const columns = [
+        { field: 'Idx', headerName: 'ID', width: 70 },
+        { field: 'License', headerName: '라이센스', width: 330 },
+        { field: 'CreatedAt', headerName: '생성일', width: 220 },
+        {
+            field: 'ExpiredAt', headerName: '만료일', width: 220,
+            renderCell:(params) => (params.value === null ? '-' : params.value)
+        },
+        {
+          field: 'IsExpired',
+          headerName: '만료여부',
+        //   description: 'This column has a value getter and is not sortable.',
+        //   sortable: false,
+          width: 120,
+          renderCell: (params) => (params.value === 1 ? 'True' : 'False'),
+        },
+        {
+            field: 'Action', // New field for the action column
+            headerName: ' ', // Header for the new column
+            width: 150,
+            renderCell: (params) => (
+                <Button onClick={() => delLicenseBtnClick(params.row)}>
+                    버튼
+                </Button>
+            ),
+        },
+    ];
+
+    const delLicenseBtnClick = (row) => {
+        console.log(`Button clicked for ID: ${row.Idx}`);
+        // TODO : 삭제하기 API 연동
+        // Add your desired functionality here
+    };
+
+    const addLicenseBtnClick = async () => {
+        const res = await axios.post('http://localhost:5000/license');
+        console.log(res);
+        const response = await axios.get('http://localhost:5000/license');
+        setLicenseList(response.data); // 응답값을 상태에 저장
+    }
+      
+    const paginationModel = { page: 0, pageSize: 25 };
+
+    useEffect(()=>{
+        const getData = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:5000/license');
+            setLicenseList(response.data); // 응답값을 상태에 저장
+        };
+        getData();
+    },[]);
+
     return (
-        <div className='Table-container'>
-            <TableContainer component={Paper}>
-                <Table aria-label="collapsible table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell />
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
-                        <Row key={row.name} row={row} />
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+        <div className='License-Table-container'>
+            <div className='License-Button-container'><Button onClick={async() => addLicenseBtnClick()}>라이센스 추가</Button></div>
+            <Paper sx={{ height: '100%', width: '100%' }}>
+                <DataGrid
+                    rows={licenseList}
+                    columns={columns}
+                    getRowId={(row) => row.Idx}
+                    initialState={{ pagination: { paginationModel } }}
+                    pageSizeOptions={[15, 25, 50]}
+                    sx={{ border: 0 }}
+            />
+            </Paper>
         </div>
     );
 }
